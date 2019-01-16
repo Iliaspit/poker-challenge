6588f6a73b07c70e1d897fd04b80511ac1b48cbc
Ilias Pittas <ipittas@worldremit.com>
2019-01-16 03:22:04 +0000

    add onClick functionality and selected property to Cards on Deck with testing

--- src/Components/Deck.js
+++ src/Components/Deck.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Card } from "../Styles/Styled";
 
-const Deck = ({ suits, values }) => (
+const Deck = ({ suits, values, selectedKeys, onClickCard }) => (
 	<>
 		{suits.map(suit => (
 			<div key={suit}>
 				{values.map(value => (
-					<Card key={suit+value} suit={suit} value={value}>
+					<Card 
+						key={suit+value}
+						suit={suit} 
+						value={value} 
+						selected={selectedKeys.includes(suit+value)}
+						onClick={() => onClickCard(suit+value)}>
 						{value}
 					</Card>
 				))}
--- src/Components/Deck.test.js
+++ src/Components/Deck.test.js
@@ -1,6 +1,6 @@
-import { mount } from "enzyme";
-
 import React from "react";
+
+import { shallow } from "enzyme";
 
 import Deck from "./Deck";
 import { Card } from "../Styles/Styled";
@@ -8,8 +8,48 @@ import { Card } from "../Styles/Styled";
 import { suits, values } from "../utils";
 
 describe(`Card deck`, () => {
+	let deck;
+	let selectedKeys;
+
+	beforeEach(() => {
+		selectedKeys = ["DA", "DK", "DQ", "DJ", "DT"];
+		deck = shallow(
+			<Deck 
+				suits={suits} 
+				values={values} 
+				selectedKeys={selectedKeys}
+				onClickCard={jest.fn()}  
+			/>
+		)
+	})
+
+	afterEach(() => {
+		deck = null
+	})
+
 	test('renders the right amount of cards', () => {
-		const deck = mount(<Deck suits={suits} values={values} />)
 		expect(deck.find(Card)).toHaveLength(52);
 	});
+
+	test('displays correct number of selected cards', () => {
+		const numberOfSelectedCard = deck.find(Card)
+										.findWhere(
+											 card => card.prop('selected') === true
+										);
+		expect(numberOfSelectedCard.length).toEqual(selectedKeys.length);
+	});
+
+	test('renders cards with the correct props', () => {
+		const expectedProps = { 
+			suit: 'D',
+			value: '2',
+			selected: false
+		};
+
+		const firstRenderedCardProps = (deck.find(Card).first().props());
+
+		expect(firstRenderedCardProps.suit).toEqual(expectedProps.suit)
+		expect(firstRenderedCardProps.value).toEqual(expectedProps.value)
+		expect(firstRenderedCardProps.selected).toEqual(expectedProps.selected)
+	})
 });
