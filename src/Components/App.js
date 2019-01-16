011fe014ab8b1290dccf3d52da7d7cb0698e1cfd
Ilias Pittas <ipittas@worldremit.com>
2019-01-16 03:19:52 +0000

    adds addNewPlayer, editPlayer, clickOnCard, removePlayer, findTheWinner and renderPlayers functionality with some tests

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,189 @@
 import React, { Component } from 'react';
 
-import { suits, values } from "../utils";
+import { suits, values, generateNewPlayer, stringifyArray } from "../utils";
 
 import Layout from "./Layout";
 import Deck from "./Deck";
-import { Card, PlayerHand, Button, Footer } from "../Styles/Styled";
+import { Button, Footer } from "../Styles/Styled";
+import Player from './Player';
+const poker = require('poker-hands');
 
 class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			players: [
+				{
+					name: 'Ilias',
+					suits: ['D'],
+					values: ["A", "K", "Q", "J", "T"],
+					keys: ["DA", "DK", "DQ", "DJ", "DT"]
+				},
+				{
+					name: 'Germain',
+					suits: ['S'],
+					values: ["A", "K", "Q", "J", "T"],
+					keys: ["SA", "SK", "SQ", "SJ", "ST"]
+				}
+			],
+			selectedKeys: [],
+			nameOfPlayerBeingEdited: null
+		}
+
+		this.addNewPlayer = this.addNewPlayer.bind(this);
+		this.renderPlayers = this.renderPlayers.bind(this);
+		this.addNewPlayer = this.addNewPlayer.bind(this);
+		this.removePlayer = this.removePlayer.bind(this);
+		this.addCardToHand = this.addCardToHand.bind(this);
+		this.removeCardFromHand = this.removeCardFromHand.bind(this);
+		this.getSelectedKeys = this.getSelectedKeys.bind(this);
+		this.editPlayer = this.editPlayer.bind(this);
+		this.clickOnCard = this.clickOnCard.bind(this);
+		this.findTheWinner = this.findTheWinner.bind(this);
+	}
+
+	componentDidMount() {
+		const allKeys = this.getSelectedKeys();
+		this.setState({
+			selectedKeys: allKeys
+		});
+	}
+
+	getSelectedKeys(players=this.state.players) {
+		const allSelectedKeys = players.map(player => (player.keys));
+		return allSelectedKeys.reduce((accumulator, currentValue) => accumulator.concat(currentValue));
+	}
+	
+	addNewPlayer() {
+		const {players} = this.state;
+		if (players.length < 6) {
+			const namesSelected = players.map(player => player.name);
+			const newPlayer = generateNewPlayer(namesSelected, this.state.selectedKeys);
+			const playersUpdated = players.concat(newPlayer)
+			const selectedKeys = this.getSelectedKeys();
+			const selectedKeysUpdated = selectedKeys.concat(newPlayer.keys);
+			
+			this.setState({
+				players: playersUpdated,
+				selectedKeys: selectedKeysUpdated
+			});
+		}
+	}
+
+	editPlayer(name) {
+		const {nameOfPlayerBeingEdited} = this.state;
+		const updatedName = nameOfPlayerBeingEdited === name ? null : name
+		this.setState({
+			nameOfPlayerBeingEdited: updatedName
+		});
+	}
+
+	removeCardFromHand(players, nameOfPlayerBeingEdited, cardKey, cardValue) {
+		return players.map(player => {
+			if (player.name === nameOfPlayerBeingEdited) {
+				player.keys = player.keys.filter(key => key !== cardKey);
+				player.values = player.values.filter(value => value !== cardValue);
+			}
+			return player
+		})
+	}
+
+	addCardToHand(players, nameOfPlayerBeingEdited, cardKey, cardValue) {
+		return players.map(player => {
+			if (player.name === nameOfPlayerBeingEdited) {
+				player.keys = player.keys.concat(cardKey);
+				player.values = player.values.concat(cardValue);
+			}
+			return player
+		})
+	}
+
+
+	clickOnCard(cardKey) {
+		let selectedKeysUpdated;
+		let playersUpdated;
+		const {players, nameOfPlayerBeingEdited} = this.state;
+		if (!nameOfPlayerBeingEdited) return null;
+
+		const playerBeingEdited = players.find(player => player.name === nameOfPlayerBeingEdited);
+		const editedPlayerHasCard = playerBeingEdited && playerBeingEdited.keys.includes(cardKey);
+		const editedPlayerHandLength = playerBeingEdited && playerBeingEdited.keys.length;
+		const cardValue = cardKey.charAt(1);
+		const selectedKeys = this.getSelectedKeys();
+		if (selectedKeys.includes(cardKey) && editedPlayerHasCard) {
+			// unselect it on Deck
+			selectedKeysUpdated = selectedKeys.filter(key => key !== cardKey);			
+			playersUpdated = this.removeCardFromHand(players, nameOfPlayerBeingEdited, cardKey, cardValue)
+		} else if (!selectedKeys.includes(cardKey) && editedPlayerHandLength < 5) {
+			// select it on Deck
+			selectedKeysUpdated = selectedKeys.concat(cardKey);
+			playersUpdated = this.addCardToHand(players, nameOfPlayerBeingEdited, cardKey, cardValue)
+		}
+
+		if (selectedKeysUpdated && playersUpdated) {
+			this.setState({
+				selectedKeys: selectedKeysUpdated,
+				players: playersUpdated
+			});
+		}
+	}
+
+	removePlayer(name) {
+		const {players} = this.state;
+		const updatedPlayers = players.filter(player => (player.name !== name));
+		const selectedKeysUpdated = this.getSelectedKeys(updatedPlayers);
+ 		if (players.length > 2) {
+			this.setState({
+				players: updatedPlayers,
+				selectedKeys: selectedKeysUpdated
+			});
+		}
+	}
+
+	findTheWinner() {
+		let winner;
+		const {players} = this.state;
+		const playersWithoutFiveCards = players.find(player => player.keys.length !== 5 )
+		if (playersWithoutFiveCards) return alert('Please make sure all players have 5 cards');
+		loop1:
+			for (const player of players) {
+				const playerHand = stringifyArray(player.keys);
+				for (var i = 1; i < players.length; i++) {
+					const compPlayerHandStr = stringifyArray(players[i].keys);
+					const winnerIndex = poker.judgeWinner([playerHand, compPlayerHandStr]);
+					if (winnerIndex === 1) {
+						break;
+					}
+					if (i === players.length - 1) { 
+						winner = player.name;
+						alert(`The winner is ${winner}`)
+						break loop1;
+					}
+				}
+			}
+		return winner;
+	}
+
+	renderPlayers() {
+		const {players, nameOfPlayerBeingEdited} = this.state
+		return players.map(player => {
+			const {name, keys} = player;
+			return (
+				<Player
+					key={name+keys.join()}
+					name={name} 
+					keys={keys}
+					onClickRemove={this.removePlayer}
+					onClickEdit={this.editPlayer}
+					isPlayerSelected={name === nameOfPlayerBeingEdited}
+					onClickOnCard={this.clickOnCard}
+				/>
+			);
+		})
+	}
+
 	render() {
 		return (
 				<Layout>
@@ -15,80 +192,26 @@ class App extends Component {
 						<h1>
 						Cards deck
 						</h1>
-						<Deck suits={suits} values={values} />
+						<Deck 
+							suits={suits} 
+							values={values} 
+							selectedKeys={this.state.selectedKeys}
+							onClickCard={this.clickOnCard}
+						/>
 					</section>
 					<section>
 						<header>
 							<h1>Players</h1>
 						</header>
 						<section>
-							<article>
-								<p>
-									Player 1 name
-									<Button>
-										<span role="img" alt="pencil" aria-label="pencil">✏️</span>
-										Edit
-									</Button>
-									<Button>
-										<span role="img" alt="flame" aria-label="flame">🔥</span>
-										Remove
-									</Button>
-								</p>
-								<PlayerHand>
-										<Card suit="D" value="A" selected={true}>
-											A
-										</Card>
-										<Card suit="D" value="K">
-											K
-										</Card>
-										<Card suit="D" value="Q">
-											Q
-										</Card>
-										<Card suit="D" value="J">
-											J
-										</Card>
-										<Card suit="D" value="T">
-											T
-										</Card>
-								</PlayerHand>
-							</article>
-							<article>
-								<p>
-									Player 2 name
-									<Button>
-										<span role="img" alt="pencil" aria-label="pencil">✏️</span>
-										Edit
-									</Button>
-									<Button>
-										<span role="img" alt="flame" aria-label="flame">🔥</span>
-										Remove
-									</Button>
-								</p>
-								<PlayerHand>
-										<Card suit="S" value="A">
-											A
-										</Card>
-										<Card suit="S" value="K">
-											K
-										</Card>
-										<Card suit="S" value="Q" selected={true}>
-											Q
-										</Card>
-										<Card suit="S" value="J">
-											J
-										</Card>
-										<Card suit="S" value="T">
-											T
-										</Card>
-								</PlayerHand>
-							</article>
+							{this.renderPlayers()}
 						</section>
 						<Footer>
-								<Button>
+								<Button onClick={() => this.addNewPlayer()}>
 									<span role="img" alt="woman raising hand" aria-label="woman raising hand">🙋‍♀️</span>
 									Add new player
 								</Button>
-								<Button>
+								<Button onClick={() => this.findTheWinner()}>
 									<span role="img" alt="trophy" aria-label="trophy">🏆</span>
 									Find the winner
 								</Button>
--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -1,9 +1,214 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import sinon from 'sinon';
+import { shallow } from "enzyme";
+
+import App from './App';
+import Layout from "./Layout";
+import Deck from "./Deck";
+import { Button, Footer } from "../Styles/Styled";
+import Player from './Player';
+
+const poker = require('poker-hands');
+
+describe(`App`, () => {
+
+  describe('Renders' , () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  })
+
+  describe('App functionality', () => {
+    let appShallowWrapper;
+    let sandbox;
+
+    beforeEach(() => {
+      appShallowWrapper = shallow(<App />);
+      appShallowWrapper.setState({
+        players: [
+          {
+            name: 'Ilias',
+            suits: ['D'],
+            values: ["A", "K", "Q", "J", "T"],
+            keys: ["DA", "DK", "DQ", "DJ", "DT"]
+          },
+          {
+            name: 'Germain',
+            suits: ['S'],
+            values: ["A", "K", "Q", "J", "T"],
+            keys: ["SA", "SK", "SQ", "SJ", "ST"]
+          }
+        ],
+        selectedKeys: [
+          "DA", "DK", "DQ", "DJ", "DT", "SA", "SK", "SQ", "SJ", "ST"
+        ],
+        nameOfPlayerBeingEdited: null
+      })
+      sandbox = sinon.createSandbox();
+    })
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it('renders the correct components', () => {
+      const numberOfPlayers = appShallowWrapper.state().players.length;
+
+      expect(appShallowWrapper.find(Layout).length).toEqual(1);
+      expect(appShallowWrapper.find(Deck).length).toEqual(1);
+      expect(appShallowWrapper.find(Button).length).toEqual(2);
+      expect(appShallowWrapper.find(Footer).length).toEqual(1);
+      expect(appShallowWrapper.find(Player).length).toEqual(numberOfPlayers);
+    })
+
+    it('should set the selectedKeys state correctly when component mounts', () => {
+      appShallowWrapper.instance().componentDidMount();
+      const state = appShallowWrapper.state();
+      const selectedKeys = state.players.map(player => player.keys);
+      const selectedKeysConcat = [...selectedKeys[0], ...selectedKeys[1]]
+      expect(state.selectedKeys).toEqual(selectedKeysConcat);
+    });
+
+    it('should call addNewPlayer when addNewPlayer button is clicked', () => {
+      const addNewPlayerSpy = sandbox.spy(appShallowWrapper.instance(), 'addNewPlayer');
+      appShallowWrapper.find(Button).at(0).simulate('click');
+      expect(addNewPlayerSpy).toHaveProperty('callCount', 1);
+    })
+
+    it('should update the state when new player is added', () => {
+      const initialNumOfPlayers = appShallowWrapper.state().players.length;
+      const initialNumOfSelectedKeys = appShallowWrapper.state().selectedKeys.length;
+      
+      appShallowWrapper.instance().addNewPlayer();
+
+      const updatedNumOfPlayers = appShallowWrapper.state().players.length;
+      const updatedNumOfSelectedKeys = appShallowWrapper.state().selectedKeys.length;
+      
+      expect(updatedNumOfPlayers).toEqual(initialNumOfPlayers + 1)
+      expect(updatedNumOfSelectedKeys).toEqual(initialNumOfSelectedKeys + 5)
+    })
+
+    it('should not add new player if number of player is already 6', () => {
+      appShallowWrapper.instance().addNewPlayer();
+      appShallowWrapper.instance().addNewPlayer();
+      appShallowWrapper.instance().addNewPlayer();
+      appShallowWrapper.instance().addNewPlayer();
+      appShallowWrapper.instance().addNewPlayer();
+      appShallowWrapper.instance().addNewPlayer();
+      appShallowWrapper.instance().addNewPlayer();
+
+      const updatedNumOfPlayers = appShallowWrapper.state().players.length;
+      const updatedNumOfSelectedKeys = appShallowWrapper.state().selectedKeys.length;
+      
+      expect(updatedNumOfPlayers).toEqual(6)
+      expect(updatedNumOfSelectedKeys).toEqual(30)
+    })
+
+    it('should update correctly state property nameOfPlayerBeingEdited when player edited', () => {
+      let nameOfPlayerBeingEdited;
+      const expectedName = 'some-name';
+
+      appShallowWrapper.instance().editPlayer(expectedName);
+
+      nameOfPlayerBeingEdited = appShallowWrapper.state().nameOfPlayerBeingEdited;
+      
+      expect(nameOfPlayerBeingEdited).toEqual(expectedName);
+      appShallowWrapper.instance().editPlayer(expectedName);
+
+      nameOfPlayerBeingEdited = appShallowWrapper.state().nameOfPlayerBeingEdited;
+      expect(nameOfPlayerBeingEdited).toEqual(null);
+    })
+
+    it('should update player and selectedKeys state properties when a player is removed', () => {
+      const expectedSelectedKeys = ["DA", "DK", "DQ", "DJ", "DT", "SA", "SK", "SQ", "SJ", "ST"];
+      appShallowWrapper.setState({
+        players: [
+          {
+            name: 'Ilias',
+            suits: ['D'],
+            values: ["A", "K", "Q", "J", "T"],
+            keys: ["DA", "DK", "DQ", "DJ", "DT"]
+          },
+          {
+            name: 'Germain',
+            suits: ['S'],
+            values: ["A", "K", "Q", "J", "T"],
+            keys: ["SA", "SK", "SQ", "SJ", "ST"]
+          },
+          {
+            name: 'Tom',
+            suits: ['M'],
+            values: ["2", "K", "Q", "4", "T"],
+            keys: ["M2", "MK", "MQ", "M4", "MT"]
+          },
+        ],
+        selectedKeys: [
+          "DA", "DK", "DQ", "DJ", "DT", "SA", "SK", "SQ", "SJ", "ST", "M2", "MK", "MQ", "M4", "MT"
+        ],
+        nameOfPlayerBeingEdited: null
+      })
+
+      appShallowWrapper.instance().removePlayer('Tom');
+
+      const selectedKeysUpdated = appShallowWrapper.state().selectedKeys;
+      const playerNames = appShallowWrapper.state().players.map(player => player.name);
+
+      expect(selectedKeysUpdated).toEqual(expectedSelectedKeys)
+      expect(playerNames).toEqual(['Ilias', 'Germain'])
+    })
+
+    it('should not remove player and update state when the number of players is 2', () => {
+      const expectedSelectedKeys = ["DA", "DK", "DQ", "DJ", "DT", "SA", "SK", "SQ", "SJ", "ST"];
+
+      appShallowWrapper.instance().removePlayer('Ilias');
+
+      const selectedKeysUpdated = appShallowWrapper.state().selectedKeys;
+      const playerNames = appShallowWrapper.state().players.map(player => player.name);
+
+      expect(selectedKeysUpdated).toEqual(expectedSelectedKeys)
+      expect(playerNames).toEqual(['Ilias', 'Germain'])
+    })
+
+    it('should call find the winner when findTheWinner button is clicked', () => {
+      const findTheWinnerSpy = sandbox.spy(appShallowWrapper.instance(), 'findTheWinner');
+      appShallowWrapper.find(Button).at(1).simulate('click');
+      expect(findTheWinnerSpy).toHaveProperty('callCount', 1);
+    })
+
+    it('should return the winner when every player has 5 cards on hand', () => {
+      sandbox.stub(poker, 'judgeWinner').returns(0);
+      const expectedWinnerName = appShallowWrapper.state().players[0].name;
+      const winnerName = appShallowWrapper.instance().findTheWinner();
+      expect(winnerName).toEqual(expectedWinnerName);
+    })
+
+    it('should show alert and not return the winner when any player has less than 5 cards on hand', () => {
+      appShallowWrapper.setState({
+        players: [
+          {
+            name: 'Ilias',
+            suits: ['D'],
+            values: ["A", "K", "Q", "J", "T"],
+            keys: ["DA", "DK", "DQ", "DJ", "DT"]
+          },
+          {
+            name: 'Germain',
+            suits: ['S'],
+            values: ["A", "K", "Q"],
+            keys: ["SA", "SK", "SQ"]
+          }
+        ],
+        selectedKeys: [
+          "DA", "DK", "DQ", "DJ", "DT", "SA", "SK", "SQ"
+        ],
+        nameOfPlayerBeingEdited: null
+      })
+      const winnerName = appShallowWrapper.instance().findTheWinner();
+      expect(winnerName).toEqual(undefined);
+    })
+  })
+})
